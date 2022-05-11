587d664079c81b0ccb4de7ec5c8ce5894e136d51
2019082851 <2019082851@hanyang.ac.kr>
2022-05-12 03:13:24 +0900

    assignment7

---
+++ assignment7/2019082851-7-1.py
@@ -0,0 +1,280 @@
+import glfw
+import sys
+import pdb
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.arrays import ArrayDatatype
+import time
+import numpy as np
+import ctypes
+from PIL.Image import open
+import OBJ
+from scipy.spatial.transform import Rotation 
+
+# global variables
+wld2cam=[]
+cow2wld1=None
+cow2wld2=None
+floorTexID=0
+cameras= [
+	[28,18,28, 0,2,0, 0,1,0],   
+	[28,18,-28, 0,2,0, 0,1,0], 
+	[-28,18,28, 0,2,0, 0,1,0], 
+	[-12,12,0, 0,2,0, 0,1,0],  
+	[0,100,0,  0,0,0, 1,0,0]
+]
+cowModel=None
+
+def vector3(x,y,z):
+    return np.array((x,y,z))
+
+def position3(v):
+    # divide by w
+    w=v[3]
+    return vector3(v[0]/w, v[1]/w, v[2]/w)
+
+def vector4(x,y,z):
+    return np.array((x,y,z,1))
+
+def matrix4(R, v):
+    T=np.eye(4)
+    T[0:3, 0:3]=R
+    setTranslation(T, v)
+    return T
+
+def rotate(m,v):
+    return m[0:3, 0:3]@v
+
+def transform(m, v):
+    return position3(m@np.append(v,1))
+
+def getTranslation(m):
+    return m[0:3,3]
+
+def setTranslation(m,v):
+    m[0:3,3]=v
+
+
+def onKeyPress( window, key, scancode, action, mods):
+    global cameraIndex
+    if action==glfw.RELEASE:
+        return ; # do nothing
+    # If 'c' or space bar are pressed, alter the camera.
+    # If a number is pressed, alter the camera corresponding the number.
+    if key==glfw.KEY_C or key==glfw.KEY_SPACE:
+        print( "Toggle camera %s\n"% cameraIndex );
+        cameraIndex += 1;
+
+    if cameraIndex >= len(wld2cam):
+        cameraIndex = 0;
+
+
+def drawFrame(leng):
+    glDisable(GL_LIGHTING);	# Lighting is not needed for drawing axis.
+    glBegin(GL_LINES);		# Start drawing lines.
+    glColor3d(1,0,0);		# color of x-axis is red.
+    glVertex3d(0,0,0);			
+    glVertex3d(leng,0,0);	# Draw line(x-axis) from (0,0,0) to (len, 0, 0). 
+    glColor3d(0,1,0);		# color of y-axis is green.
+    glVertex3d(0,0,0);			
+    glVertex3d(0,leng,0);	# Draw line(y-axis) from (0,0,0) to (0, len, 0).
+    glColor3d(0,0,1);		# color of z-axis is  blue.
+    glVertex3d(0,0,0);
+    glVertex3d(0,0,leng);	# Draw line(z-axis) from (0,0,0) - (0, 0, len).
+    glEnd();			# End drawing lines.
+
+#*********************************************************************************
+# Draw 'cow' object.
+#*********************************************************************************/
+def drawCow(_cow2wld):
+
+    glPushMatrix();		# Push the current matrix of GL into stack. This is because the matrix of GL will be change while drawing cow.
+
+    # The information about location of cow to be drawn is stored in cow2wld matrix.
+    # (Project2 hint) If you change the value of the cow2wld matrix or the current matrix, cow would rotate or move.
+    glMultMatrixd(_cow2wld.T)
+
+    drawFrame(5);										# Draw x, y, and z axis.
+    frontColor = [0.8, 0.2, 0.9, 1.0];
+    glEnable(GL_LIGHTING);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, frontColor);		# Set ambient property frontColor.
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, frontColor);		# Set diffuse property frontColor.
+    cowModel.render()	# Draw cow. 
+    glDisable(GL_LIGHTING);
+
+    glPopMatrix();			# Pop the matrix in stack to GL. Change it the matrix before drawing cow.
+def drawFloor():
+
+    glDisable(GL_LIGHTING);
+
+    # Set color of the floor.
+    # Assign checker-patterned texture.
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, floorTexID );
+
+    # Draw the floor. Match the texture's coordinates and the floor's coordinates resp. 
+    nrep=4
+    glBegin(GL_POLYGON);
+    glTexCoord2d(0,0);
+    glVertex3d(-12,-0.1,-12);		# Texture's (0,0) is bound to (-12,-0.1,-12).
+    glTexCoord2d(nrep,0);
+    glVertex3d( 12,-0.1,-12);		# Texture's (1,0) is bound to (12,-0.1,-12).
+    glTexCoord2d(nrep,nrep);
+    glVertex3d( 12,-0.1, 12);		# Texture's (1,1) is bound to (12,-0.1,12).
+    glTexCoord2d(0,nrep);
+    glVertex3d(-12,-0.1, 12);		# Texture's (0,1) is bound to (-12,-0.1,12).
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D);	
+    drawFrame(5);				# Draw x, y, and z axis.
+
+def display():
+    global cameraIndex, cow2wld1, cow2wld2, animStartTime
+    glClearColor(0.8, 0.9, 0.9, 1.0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);				# Clear the screen
+    # set viewing transformation.
+    glLoadMatrixd(wld2cam[cameraIndex].T)
+
+    drawFloor();													# Draw floor.
+
+
+    # TODO: 
+    # create a new cow2wld here to animate the cow.
+    animTime=glfw.get_time()-animStartTime;
+    #print(animTime)
+    #you need to modify both the translation and rotation parts of the cow2wld matrix.
+    R1=Rotation.from_matrix(cow2wld1[0:3,0:3])
+    R2=Rotation.from_matrix(cow2wld2[0:3,0:3])
+    print(getTranslation(cow2wld1))
+    delta=(R1.inv()*R2).as_rotvec()
+    delta= delta*(0.5*np.cos(np.pi*animTime)+0.5)
+
+    
+    location = (1-(0.5*np.cos(np.pi*animTime)+0.5))*getTranslation(cow2wld1) 
+    location += (0.5*np.cos(np.pi*animTime)+0.5)*getTranslation(cow2wld2)
+    # if t = 0 ,getTranslation(cow2wld1) if t= 1 getTranslation(cow2wld2)
+
+    R3=R1*Rotation.from_rotvec(delta)
+    print('R2', R2.as_matrix())
+    print('R3', R3.as_matrix())
+    print('R2',R2.as_euler('zyx', degrees=True))
+    
+													# Draw cow.
+    drawCow(matrix4(R3.as_matrix(), location))
+
+    
+    glFlush();
+
+def reshape(window, w, h):
+    width = w;
+    height = h;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);            # Select The Projection Matrix
+    glLoadIdentity();                       # Reset The Projection Matrix
+    # Define perspective projection frustum
+    aspect = width/(float)(height);
+    gluPerspective(45, aspect, 1, 1024);
+    matProjection=glGetDoublev(GL_PROJECTION_MATRIX).T
+    glMatrixMode(GL_MODELVIEW);             # Select The Modelview Matrix
+    glLoadIdentity();                       # Reset The Projection Matrix
+
+def initialize(window):
+    global floorTexID, cameraIndex, cow2wld1, cow2wld2, cowModel, animStartTime
+    # Set up OpenGL state
+    glShadeModel(GL_SMOOTH);         # Set Smooth Shading
+    glEnable(GL_DEPTH_TEST);         # Enables Depth Testing
+    glDepthFunc(GL_LEQUAL);          # The Type Of Depth Test To Do
+    # Use perspective correct interpolation if available
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    # Initialize the matrix stacks
+    width, height = glfw.get_window_size(window)
+    reshape(window, width, height);
+    # Define lighting for the scene
+    lightDirection   = [1.0, 1.0, 1.0, 0];
+    ambientIntensity = [0.1, 0.1, 0.1, 1.0];
+    lightIntensity   = [0.9, 0.9, 0.9, 1.0];
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientIntensity);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightDirection);
+    glEnable(GL_LIGHT0);
+
+    # initialize floor
+    im = open('bricks.bmp')
+    try:
+        ix, iy, image = im.size[0], im.size[1], im.tobytes("raw", "RGB", 0, -1)
+    except SystemError:
+        ix, iy, image = im.size[0], im.size[1], im.tobytes("raw", "RGBX", 0, -1)
+
+    # Make texture which is accessible through floorTexID. 
+    floorTexID=glGenTextures( 1)
+    glBindTexture(GL_TEXTURE_2D, floorTexID);		
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, ix, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    # initialize cow
+    cowModel=OBJ.OBJrenderer("cow.obj")
+
+    # initialize cow2wld matrix
+    glPushMatrix();		        # Push the current matrix of GL into stack.
+    glLoadIdentity();		        # Set the GL matrix Identity matrix.
+    glTranslated(0,-cowModel.bbmin[1],-8);	# Set the location of cow.
+    glRotated(-90, 0, 1, 0);		# Set the direction of cow. These information are stored in the matrix of GL.
+    cow2wld1=glGetDoublev(GL_MODELVIEW_MATRIX).T # convert column-major to row-major 
+    glPopMatrix();			# Pop the matrix on stack to GL.
+
+
+    glPushMatrix();		        # Push the current matrix of GL into stack.
+    glLoadIdentity();		        # Set the GL matrix Identity matrix.
+    glTranslated(-1,-cowModel.bbmin[1]*0.5,-4);	# Set the location of cow.
+    glRotated(-90, 1, 0, 0);		# Set the direction of cow. These information are stored in the matrix of GL.
+    cow2wld2=glGetDoublev(GL_MODELVIEW_MATRIX).T # convert column-major to row-major 
+
+    glPopMatrix();			# Pop the matrix on stack to GL.
+
+
+
+
+    # initialize camera frame transforms.
+
+    cameraCount=len(cameras)
+    for i in range(cameraCount):
+        # 'c' points the coordinate of i-th camera.
+        c = cameras[i];										
+        glPushMatrix();													# Push the current matrix of GL into stack.
+        glLoadIdentity();												# Set the GL matrix Identity matrix.
+        gluLookAt(c[0],c[1],c[2], c[3],c[4],c[5], c[6],c[7],c[8]);		# Setting the coordinate of camera.
+        wld2cam.append(glGetDoublev(GL_MODELVIEW_MATRIX).T)
+        glPopMatrix();													# Transfer the matrix that was pushed the stack to GL.
+
+    cameraIndex = 0;
+    animStartTime=glfw.get_time();
+
+
+def main():
+    if not glfw.init():
+        print ('GLFW initialization failed')
+        sys.exit(-1)
+    width = 800;
+    height = 600;
+    window = glfw.create_window(width, height, '2019082851-7-1', None, None)
+    if not window:
+        glfw.terminate()
+        sys.exit(-1)
+
+    glfw.make_context_current(window)
+    glfw.set_key_callback(window, onKeyPress)
+    glfw.swap_interval(1)
+
+    initialize(window);						
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+        display()
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+if __name__ == "__main__":
+    main()
